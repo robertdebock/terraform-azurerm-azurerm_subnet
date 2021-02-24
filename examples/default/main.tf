f53b340bc3a617f4c3d9fab00c6e8d6bcda5d997
Robert de Bock <robert@meinit.nl>
2021-02-24 16:00:49 +0100

    Predictable names.

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,13 +1,13 @@
 module "azurerm_resource_group" {
   source  = "robertdebock/azurerm_resource_group/azurerm"
   version = "1.0.3"
-  name    = "test_rg"
+  name    = "test_resource_group"
 }
 
 module "azurerm_virtual_network" {
   source              = "robertdebock/azurerm_virtual_network/azurerm"
   version             = "1.0.2"
-  name                = "test_vnet"
+  name                = "test_virtual_network"
   address_space       = ["10.0.0.0/16"]
   resource_group_name = module.azurerm_resource_group.name
 }
