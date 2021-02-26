eec38df1bd3c489d71e8efbbf9ef06f7d93aebaf
Robert de Bock <robert@meinit.nl>
2021-02-26 15:20:29 +0100

    Update to azurerm 2.49.0

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,12 +1,12 @@
 module "azurerm_resource_group" {
   source  = "robertdebock/azurerm_resource_group/azurerm"
-  version = "1.0.3"
+  version = "2.0.0"
   name    = "test_resource_group"
 }
 
 module "azurerm_virtual_network" {
   source              = "robertdebock/azurerm_virtual_network/azurerm"
-  version             = "1.0.2"
+  version             = "2.0.0"
   name                = "test_virtual_network"
   address_space       = ["10.0.0.0/16"]
   resource_group_name = module.azurerm_resource_group.name
